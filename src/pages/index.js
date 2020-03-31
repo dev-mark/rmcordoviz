6bd5ad51c54e58991d5bb5f3f425e239fd915dd9
dev-mark <mark@rmcordoviz.com>
2020-03-31 09:27:05 +0800

    Remove unnecessary codes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,9 @@
 import React from "react";
 
-// components
-import Navbar from "../components/Navbar";
-import Footer1 from "../components/Footer1.js";
-import Footer2 from "../components/Footer2.js";
-
 function index() {
   return (
     <div>
-      {/* <Navbar /> */}
       <h1> HOME PAGE</h1>
-      {/* <Footer1 />
-      <Footer2 /> */}
     </div>
   );
 }
--- src/pages/zija.js
+++ src/pages/zija.js
@@ -1,17 +1,9 @@
 import React from "react";
 
-// components
-import Navbar from "../components/Navbar";
-import Footer1 from "../components/Footer1.js";
-import Footer2 from "../components/Footer2.js";
-
 function zija() {
   return (
     <div>
-      {/* <Navbar /> */}
       <h1> HEALTH AND WELLNESS</h1>
-      {/* <Footer1 />
-      <Footer2 /> */}
     </div>
   );
 }
