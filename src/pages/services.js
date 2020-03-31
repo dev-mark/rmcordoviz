bcba47dda6ab729eca7fb9dd57e599b0024fc0ac
dev-mark <mark@rmcordoviz.com>
2020-03-31 14:04:06 +0800

    Add services banner

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "../assets/styles/services.scss";
 
 function services() {
   return (
     <div>
-      <h1>SERVICES</h1>
+      <div className=" services-banner">
+        <div id="color-overlay">
+          <div className=" center-text-to-div">
+            <h1 className=" text-warning">
+              <strong>SERVICES</strong>
+            </h1>
+            <p className="  text-white">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
