986fd6de22f66eef84dd72ab3790bbe0b68e2d1f
dev-mark <mark@rmcordoviz.com>
2020-04-02 08:18:53 +0800

    Remove unnecessary codes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import "../assets/styles/home.scss";
 // bootstrap
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Carousel from "react-bootstrap/Carousel";
 
