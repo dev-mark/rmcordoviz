4a7c6e50fa5098b3a5a53fed61af25194e1c1caa
dev-mark <mark@rmcordoviz.com>
2020-04-05 21:12:48 +0800

    Enhance Responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ import logo from "../assets/images/RMC.png";
 // bootstrap
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
 
 const App = (props) => {
   const [location, setLocation] = useState("");
@@ -21,81 +20,79 @@ const App = (props) => {
       collapseOnSelect={true}
       expand="md"
       variant="dark"
-      className="justify"
+      className="justify top-layer"
     >
-      <Container>
-        <Navbar.Brand href="/">
-          <img
-            src={logo}
-            width="60"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="justify-content-end">
-            <Nav.Link>
-              <Link
-                to="/"
-                className={location === "/" && "selected-state"}
-                onClick={() => setLocation("home")}
-              >
-                Home
-              </Link>
-            </Nav.Link>
+      <Navbar.Brand href="/">
+        <img
+          src={logo}
+          width="60"
+          height="30"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav " />
+      <Navbar.Collapse id="responsive-navbar-nav ">
+        <Nav className="justify-content-end">
+          <Nav.Link>
+            <Link
+              to="/"
+              className={location === "/" && "selected-state"}
+              onClick={() => setLocation("home")}
+            >
+              Home
+            </Link>
+          </Nav.Link>
 
-            <Nav.Link>
-              <Link
-                to="/about"
-                className={location === "/about" && "selected-state"}
-                onClick={() => setLocation("about")}
-              >
-                About
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link
-                to="/services"
-                className={location === "/services" && "selected-state"}
-                onClick={() => setLocation("services")}
-              >
-                Services
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link
-                to="/contact"
-                className={location === "/contact" && "selected-state"}
-                onClick={() => setLocation("contact")}
-              >
-                Contact
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link
-                to="/coaching"
-                className={location === "/coaching" && "selected-state"}
-                onClick={() => setLocation("coaching")}
-              >
-                Coaching
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link
-                to="/health-and-wellness"
-                className={
-                  location === "/health-and-wellness" && "selected-state"
-                }
-                onClick={() => setLocation("health-and-wellness")}
-              >
-                Health and Wellness
-              </Link>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+          <Nav.Link>
+            <Link
+              to="/about"
+              className={location === "/about" && "selected-state"}
+              onClick={() => setLocation("about")}
+            >
+              About
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link
+              to="/services"
+              className={location === "/services" && "selected-state"}
+              onClick={() => setLocation("services")}
+            >
+              Services
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link
+              to="/contact"
+              className={location === "/contact" && "selected-state"}
+              onClick={() => setLocation("contact")}
+            >
+              Contact
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link
+              to="/coaching"
+              className={location === "/coaching" && "selected-state"}
+              onClick={() => setLocation("coaching")}
+            >
+              Coaching
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link
+              to="/health-and-wellness"
+              className={
+                location === "/health-and-wellness" && "selected-state"
+              }
+              onClick={() => setLocation("health-and-wellness")}
+            >
+              Health and Wellness
+            </Link>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
